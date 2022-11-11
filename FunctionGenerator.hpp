feaee016dd827fec401c8291ad851b42d053fa7f
Lxixnxuxs <linusmeierhfer@yahoo.com>
2022-11-11 16:01:21 +0100

    Changed index bug

--- FunctionGenerator.hpp
+++ FunctionGenerator.hpp
@@ -13,28 +13,20 @@ std::function<bool(int)> generator_functions[] = {[] (int n) {return ((int) std:
 
 class RandomFunction {
     int num_generator_functions;
-    int recursion_degree;
     std::vector<int> generator_functions_indx;
-    std::vector<int> generator_functions_coeffs;
-    std::function<bool(int)> f = [](int) { return 0; };
 
     int num_precomputed = 0;
     std::vector<bool> precomputed;
 
 public:
 
-    RandomFunction(int max_generator_functions, int min_coeff, int max_coeff, int max_recursion_degree) {
+    RandomFunction(int max_generator_functions) {
         RandomGenerator R;
 
-        recursion_degree = R.random_integer(0, max_recursion_degree + 1);
+        num_generator_functions = R.random_integer(1, max_generator_functions);
 
-        num_generator_functions = R.random_integer(1, max_generator_functions + 1);
-
-        int t;
         for (int i = 0; i < num_generator_functions; i++) {
-            t = R.random_integer(min_coeff, max_coeff + 1);
-            generator_functions_coeffs.push_back(t);
-            generator_functions_indx.push_back(R.random_integer(0, num_generator_functions));
+            generator_functions_indx.push_back(R.random_integer(0, num_generator_functions - 1));
         }
     }
 
